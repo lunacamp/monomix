fc487a033a559e204e0c749bce1dbd247c2af9ae
Roman Korneev <221614580+rkorneev@users.noreply.github.com>
2026-05-14 21:38:42 -0400

    Add KernelError -> Python exception mapping

--- rust/monomix-py/src/lib.rs
+++ rust/monomix-py/src/lib.rs
@@ -1,7 +1,14 @@
 use pyo3::prelude::*;
 
+mod errors;
+
 #[pymodule]
 fn _kernel(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add("__version__", env!("CARGO_PKG_VERSION"))?;
+    m.add("MonomixError", m.py().get_type_bound::<errors::MonomixError>())?;
+    m.add("ParseError", m.py().get_type_bound::<errors::ParseError>())?;
+    m.add("EvalError", m.py().get_type_bound::<errors::EvalError>())?;
+    m.add("UnsupportedError", m.py().get_type_bound::<errors::UnsupportedError>())?;
+    m.add("CrossSessionError", m.py().get_type_bound::<errors::CrossSessionError>())?;
     Ok(())
 }
